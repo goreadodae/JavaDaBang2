3c456fe4ebd9f6fc94f0b1072f4c4f4cf5fa393a
DESKTOP-ISOR6C5\leell <leell1366@gmail.com>
2018-03-17 16:57:32 +0900

    by 이승재 -로그인 화면수정(로고추가) -관리자로그인 생성(화면전환추가해야함)

--- javaDaBang/src/dabang/client/controller/LoginController.java
+++ javaDaBang/src/dabang/client/controller/LoginController.java
@@ -1,16 +1,41 @@
 package dabang.client.controller;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Properties;
 
 import dabang.client.model.Member;
 
 public class LoginController {
 	private HashMap<String, Member> member = new HashMap<String, Member>();
 	MemberController mCon = new MemberController();
+	private static Properties props = new Properties();
 	public void startLogin() {
 		mCon.loadMember();
 	}
-	
+	public boolean checkAdmin(String adminId) {
+		try {
+			props.load(new FileReader("admin.Properties"));
+			if(props.getProperty("ID").equals(adminId)) {
+
+				return true;
+			}
+			else return false;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	public boolean ckeckAdminPwd(String adminPwd) {
+		if(props.getProperty("password").equals(adminPwd)) return true;
+		else return false;
+	}
 	public boolean checkId(String userId) {
 		mCon.loadMember();
 		member = mCon.selectAll();        
--- javaDaBang/src/dabang/client/view/Login.java
+++ javaDaBang/src/dabang/client/view/Login.java
@@ -1,5 +1,6 @@
 package dabang.client.view;
 
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -7,9 +8,12 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -17,14 +21,18 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 import dabang.client.controller.LoginController;
-public  class Login extends JPanel implements ActionListener{
+public  class Login extends JPanel implements ActionListener,KeyListener{
 	private static Color bgc = new Color(246,245,239);
 	private JPanel panel1 = new JPanel();//아이디
 	private JPanel panel2 = new JPanel();//비밀번호
 	private JPanel panel3 = new JPanel();//맨위에 
 	private JPanel panel4 = new JPanel();//버튼
-	private JPanel panel5 = new JPanel();//사진반쪽(위)
-	private JPanel panel6 = new JPanel();//사진반쪽(아래)
+	private JPanel southPanel5 = new JPanel();//사진반쪽(위)
+
+	private JPanel centerPanel1 = new JPanel();
+	private JPanel centerPanel2 = new JPanel();
+	private JPanel centerPanel3 = new JPanel();
+	private JPanel centerPanelParent = new JPanel();
 
 	private JLabel label1 = new JLabel("ID");
 	private JLabel label2 = new JLabel("PW");
@@ -36,27 +44,29 @@ public  class Login extends JPanel implements ActionListener{
 	private JButton button1 = new JButton("로그인");
 	private JButton button2 = new JButton("비회원 주문");
 	private JButton button3 = new JButton("회원가입");
-	private ImageIcon picup = null;
-	private ImageIcon picdown = null;
-	private ImageIcon mainpic = null;
+	private ImageIcon logo = null;
+
+	private JComboBox loginSelect = null;
+	private String [] loginSel = new String[] {"사용자","관리자"};
 	private JPanel mainPanel = null;
-	private ImageIcon gaep  = null;
-	private ImageIcon login = null;
+
 
 	LoginController lCon = new LoginController();
-	
+
 	private int loginSuccess = 0;//0로그인 실패, 1사용자 로그인, 2관리자 로그인
-	
+	private JLabel logoLabel = new JLabel();;
+
 	public Login(JPanel mainPanel) 
 	{
-//		super("잡다방"); //이름설정
+		//		super("잡다방"); //이름설정
 		//this.setTitle("이름") 이랑 같음
 		this.setSize(1000,800); //프레임 사이즈
-//		this.setLocationRelativeTo(null); //화면 가운데로 위치설정
-//		this.setDefaultCloseOperation(EXIT_ON_CLOSE); //종료버튼시 아예다꺼버림
-//		this.setResizable(false);
+		//		this.setLocationRelativeTo(null); //화면 가운데로 위치설정
+		//		this.setDefaultCloseOperation(EXIT_ON_CLOSE); //종료버튼시 아예다꺼버림
+		//		this.setResizable(false);
 		this.mainPanel = mainPanel;
-		this.setLayout(new GridLayout(6,1));
+		this.setLayout(new BorderLayout());
+		this.addKeyListener(this);
 		this.compInit(); //사용자 정의 메소드
 
 		this.setVisible(true); //가시적으로 보여주어라
@@ -66,89 +76,103 @@ public  class Login extends JPanel implements ActionListener{
 		panel1.setBackground(bgc);
 		panel1.add(label1);//아이디
 		panel1.add(field1);//칸
-		this.add(panel1);
+		centerPanel2.add(panel1);
 	}
 	private void passwordgo()//비번
 	{
 		panel2.setBackground(bgc);
 		panel2.add(label2);//비번
 		panel2.add(field2);//비번칸
-		this.add(panel2);
+		centerPanel2.add(panel2);
 	}
 
+	private void adminClient() {
+		JPanel selectPanel = new JPanel();
+		loginSelect = new JComboBox(loginSel);
+		selectPanel.add(loginSelect);
+		selectPanel.setBackground(bgc);
+		selectPanel.setAlignmentX(JPanel.CENTER_ALIGNMENT);
+		selectPanel.setPreferredSize(new Dimension(50,30));
+		centerPanel2.add(selectPanel);
+	}
 
-
+	private void centerPan() {
+		centerPanel2.setLayout(new GridLayout(5,1));
+		centerPanelParent.add(centerPanel1);
+		centerPanelParent.add(centerPanel2);
+		centerPanelParent.add(centerPanel3);
+		centerPanel1.setBackground(bgc);
+		centerPanel2.setBackground(bgc);
+		centerPanel3.setBackground(bgc);
+		centerPanelParent.setBackground(bgc);
+		centerPanel1.setPreferredSize(new Dimension(320,600));
+		centerPanel2.setPreferredSize(new Dimension(320,600));
+		centerPanel3.setPreferredSize(new Dimension(320,600));
+		centerPanelParent.setPreferredSize(new Dimension(2000,600));
+		this.add(centerPanelParent, BorderLayout.NORTH);
+	}
 	private void compInit() {
 		lCon.startLogin();
 		picme();
+		centerPan();
 		saygo();
 		idgo();
 		passwordgo();
+		adminClient();
 		loginchang();
 		pic1();
-		pic2();
-
-
 	}
 
 
 	private void saygo() {
 		panel3.setBackground(bgc);
 		panel3.setPreferredSize(new Dimension(1,1));
-		label3.setIcon(mainpic);
+		//label3.setIcon(mainpic);
 		panel3.add(label3);
 
-		this.add(panel3);
+		centerPanel2.add(panel3);
 	}
 	private void loginchang()
 	{
 		panel4.setBackground(bgc);
-		button1.setIcon(login);
 		button1.setBackground(bgc);
 		panel4.add(button1);
 		panel4.add(button2);
 		button3.setBackground(bgc);
-		button3.setIcon(gaep);
 		panel4.add(button3);
 		button1.addActionListener(this);
 		button2.addActionListener(this);
 		button3.addActionListener(this);
-		
 
 
-		this.add(panel4);
+
+		centerPanel2.add(panel4);
 	}
 	private void pic1()
 	{
-		panel5.setBackground(bgc);
-		panel5.setPreferredSize(new Dimension(1,1));
-		label4.setIcon(picup);
-		panel5.add(label4);
+		southPanel5.setBackground(bgc);
+		southPanel5.setPreferredSize(new Dimension(1000,200));
+		//label4.setIcon(picup);
+		southPanel5.add(label4);
 
-		this.add(panel5);
+		this.add(southPanel5,BorderLayout.SOUTH);
 
 	}
-	private void pic2()
-	{
-		panel6.setBackground(bgc);
-		panel6.setPreferredSize(new Dimension(1,1));
-		label5.setIcon(picdown);
-		panel6.add(label5);
 
-
-		this.add(panel6);
-	}
 	private void picme()
 	{
-		mainpic = new ImageIcon(new ImageIcon("400x1222.jpg").getImage().getScaledInstance(100,90,Image.SCALE_DEFAULT));
-		picup = new ImageIcon(new ImageIcon("1-1.jpg").getImage().getScaledInstance(1000,130,Image.SCALE_DEFAULT));
-		picdown = new ImageIcon(new ImageIcon("1-2.jpg").getImage().getScaledInstance(1000,130,Image.SCALE_DEFAULT));
-		gaep = new ImageIcon(new ImageIcon("gaep.jpg").getImage().getScaledInstance(150,30,Image.SCALE_DEFAULT));
-		login = new ImageIcon(new ImageIcon("login.jpg").getImage().getScaledInstance(150,30,Image.SCALE_DEFAULT));
+		logo = new ImageIcon(new ImageIcon("Image\\LoginImage\\logo.png").getImage().getScaledInstance(300,300,Image.SCALE_DEFAULT));
+		JLabel logoLabel = new JLabel("asdf");
+		logoLabel.setIcon(logo);
+		logoLabel.setPreferredSize(new Dimension(300,300));
+		//		logoPanel.setBackground(Color.black);
+		centerPanel1.add(logoLabel);
 	}
 	public int isLogin() {
 		return loginSuccess;
 	}
+	
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()==button3)
@@ -156,12 +180,24 @@ public  class Login extends JPanel implements ActionListener{
 			new Application(lCon);
 		}
 		else if(e.getSource()==button1) {
-			if(field1.getText().length()==0) JOptionPane.showMessageDialog(null,"ID를 입력하여주세요");
-			else {
-				if(lCon.checkId(field1.getText())) {
-					if(lCon.checkPwd(field1.getText(), field2.getText())) {
+			if(loginSelect.getSelectedItem()=="사용자") {
+				if(field1.getText().length()==0) JOptionPane.showMessageDialog(null,"ID를 입력하여주세요");
+				else {
+					if(lCon.checkId(field1.getText())) {
+						if(lCon.checkPwd(field1.getText(), field2.getText())) {
+							JOptionPane.showMessageDialog(null,"로그인 성공");
+							((CardLayout)mainPanel.getLayout()).show(mainPanel, "memberMain");
+						}
+						else JOptionPane.showMessageDialog(null,"비밀번호를 확인하세요");
+					}
+					
+				}
+			}
+			else if(loginSelect.getSelectedItem()=="관리자") {
+				if(lCon.checkAdmin(field1.getText())) {
+					if(lCon.ckeckAdminPwd(field2.getText())) {
 						JOptionPane.showMessageDialog(null,"로그인 성공");
-						((CardLayout)mainPanel.getLayout()).show(mainPanel, "memberMain");
+						((CardLayout)mainPanel.getLayout()).show(mainPanel, "memberMain");//관리자메인으로 변경해야함
 					}
 					else JOptionPane.showMessageDialog(null,"비밀번호를 확인하세요");
 				}
@@ -170,5 +206,23 @@ public  class Login extends JPanel implements ActionListener{
 		}
 
 
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println(e.getKeyCode());
+		if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+			button1.doClick();
+		}
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
