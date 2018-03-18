27b6bb5673220e850221a0bc8633759e0808452f
김예진 <@DESKTOP-OR0C045>
2018-03-19 03:05:58 +0900

    GuiAllmenu menu properties 전체출력 완료 by.예진

--- javaDaBang/src/dabang/client/view/GuiAllmenu.java
+++ javaDaBang/src/dabang/client/view/GuiAllmenu.java
@@ -7,8 +7,16 @@ import java.awt.Font;
 import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Properties;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -19,6 +27,7 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 import dabang.client.controller.MenuManageControl;
+import dabang.client.model.MenuDrink;
 import dabang.client.model.MenuManage;
 
 
@@ -31,11 +40,14 @@ public class GuiAllmenu extends JPanel implements ActionListener{
 	private JButton back = new JButton("뒤로가기");
 
 	private MenuManageControl mCon = new MenuManageControl();
-	 ArrayList<String> menuname = null;
-	 ArrayList<String> menuprice = null;
+	ArrayList<String> menuname = null;
+	ArrayList<String> menuprice = null;
 	private MenuManage menuInsert ;
-	
+
 	private JPanel mainPanel = null;
+
+	private Properties props = new Properties();
+	private MenuDrink md = null;
 
 	public void p1 () { //제목
 		p1.setSize(980,90);
@@ -67,14 +79,14 @@ public class GuiAllmenu extends JPanel implements ActionListener{
 		JScrollPane allmenuscroll = new JScrollPane(allmenutable);
 		p2.add(allmenuscroll);
 
-		
+
 		String allmenu [] = new String[2];
 		for(int i=0;i<menuname.size();i++) {
 			allmenu[0]=menuname.get(i);
 			allmenu[1]=menuprice.get(i);
 			allmenumodel.addRow(allmenu);
 		}
-		
+
 	}
 
 	public void p3 () { //뒤로가기
@@ -88,7 +100,7 @@ public class GuiAllmenu extends JPanel implements ActionListener{
 		p3.add(back,BorderLayout.CENTER);
 		back.setFont(new Font("Serif",Font.BOLD,30));
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()==back) {
@@ -98,12 +110,31 @@ public class GuiAllmenu extends JPanel implements ActionListener{
 
 	private void comInit() {
 
-		mCon.menuPlus(new MenuManage("아메리카노",200));
-		mCon.menuPlus(new MenuManage("카페라떼",10000));
-		mCon.menuPlus(new MenuManage("카페모카",900));
-		mCon.menuPlus(new MenuManage("딸기",2000));
-		mCon.menuPlus(new MenuManage("아메아메",500));
 
+		String str  =  null;
+		String s[] = null;
+		String showPrice = null;
+
+		try(BufferedInputStream bfs = new BufferedInputStream(new FileInputStream("menu.properties"))) {
+			props.load(bfs);
+
+			Enumeration<Object> enumm = props.keys();
+
+			while (enumm.hasMoreElements()) {
+				String key = (String) enumm.nextElement(); 
+				str = new String(((String) props.get(key)).getBytes("ISO-8859-1"), "UTF-8");
+
+				s = str.split("/");
+
+				mCon.menuPlus(new MenuManage(s[0],Integer.parseInt(s[3])));
+			}
+
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		menuname = mCon.arraytmenuname();
 		menuprice = mCon.arraytmenuprice();
