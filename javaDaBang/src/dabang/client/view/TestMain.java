20fabc4af06eef3bc1b9ce3570f2d2610ff05f2e
DESKTOP-ISOR6C5\leell <leell1366@gmail.com>
2018-03-20 13:43:00 +0900

    진짜 최종

--- javaDaBang/src/dabang/client/view/TestMain.java
+++ javaDaBang/src/dabang/client/view/TestMain.java
@@ -7,20 +7,16 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		try {
-		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-		        if ("Nimbus".equals(info.getName())) {
-		            UIManager.setLookAndFeel(info.getClassName());
-		            break;
-		        }
-		    }
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
 		} catch (Exception e) {
-		    // If Nimbus is not available, you can set the GUI to another look and feel.
 		}
-//		MemberView mv = new MemberView();
-//		mv.mainMenu();
-		//new ViewMain();
 		new MainView();
-//		new OrderView();
+
 	}
 
 }
